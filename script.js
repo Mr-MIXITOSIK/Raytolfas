a4525fed51cd903afa2d8d67226b7760bceb7a58
Mr-MIXITOSIK <hkr8376jgg@gmail.com>
2024-08-30 16:47:44 +0300

    script.js

--- script.js
+++ script.js
@@ -12,17 +12,26 @@ document.getElementById('link-form').addEventListener('submit', function(e) {
     .then(data => {
         const shortUrl = data.shortUrl;
         const shortLinkElement = document.getElementById('short-link');
-        
+
         shortLinkElement.href = shortUrl;
         shortLinkElement.textContent = shortUrl;
         document.getElementById('result').style.display = 'block';
 
-        // Копируем короткую ссылку в буфер обмена
-        navigator.clipboard.writeText(shortUrl).then(() => {
-            alert('Ссылка скопирована в буфер обмена: ' + shortUrl);
-        }).catch(err => {
-            console.error('Ошибка копирования ссылки:', err);
-        });
+        // Копирование короткой ссылки в буфер обмена
+        copyToClipboard(shortUrl);
     })
     .catch(error => console.error('Ошибка:', error));
 });
+
+// Функция копирования текста в буфер обмена
+function copyToClipboard(text) {
+    // Создаем временный элемент для копирования текста
+    const tempInput = document.createElement('input');
+    document.body.appendChild(tempInput);
+    tempInput.value = text;
+    tempInput.select();
+    document.execCommand('copy');
+    document.body.removeChild(tempInput);
+
+    alert('Ссылка скопирована в буфер обмена: ' + text);
+}
